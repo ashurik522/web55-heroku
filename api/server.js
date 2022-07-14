78f5ddefb15c6802c3ec1af4e96dfc7d8447c3ff
ashurik522 <ashurik522@gmail.com>
2022-07-14 12:53:06 -0400

    wip

--- api/server.js
+++ api/server.js
@@ -5,7 +5,13 @@ const server = express();
 server.use(express.json())
 
 server.get('/', (req, res) => {
-    res.send('<h1>Ther Server is Up!</h1>')
+    res.send('<h1>The Server is Up!</h1>')
 })
+
+server.get('/message-of-the-day', (req, res) => {
+    res.send(`<h1>Today's Message </h1><p>${process.env.MOTD || '(no message)'} </p>`)
+})
+
+
 
 module.exports = server
